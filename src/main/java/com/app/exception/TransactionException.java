18ae5568ee58e90087651821aa13c8eea8b863e2
Cayuro <juan_gomez2000@hotmail.com>
2026-04-27 13:00:16 -0500

    transaction exception overloading with the message only constructor

--- src/main/java/com/app/exception/TransactionException.java
+++ src/main/java/com/app/exception/TransactionException.java
@@ -1,6 +1,10 @@
 package com.app.exception;
 
 public class TransactionException extends BusinessException {
+    public TransactionException(String message) {
+        super(message);
+    }
+
     public TransactionException(String message, Throwable cause) {
         super(message, cause);
     }
